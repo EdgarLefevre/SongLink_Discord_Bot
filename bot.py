274e9ddca402ce0c0c5e98ddd020836cdbe99c88
Edgar <lefevreedg@gmail.com>
2022-02-16 10:33:20 +0100

    :construction: :bug: Link list cannot be None, Try catch on each link

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import os
-import random
 import requests
 
 from discord.ext import commands
@@ -10,38 +9,34 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
-PLATFORMS=["appleMusic", 'spotify', "deezer", 'youtubeMusic']
+PLATFORMS = ["appleMusic", 'spotify', "deezer", 'youtubeMusic']
+
 
 def get_links(url):
-    links=[]
-    api_link = "https://api.song.link/v1-alpha.1/links?url="+url+"&userCountry=US"
+    links = []
+    api_link = "https://api.song.link/v1-alpha.1/links?url=" + url + "&userCountry=US"
     response = requests.get(api_link)
-    try:
-        for p in PLATFORMS:
+    for p in PLATFORMS:
+        try:
             links.append(response.json()['linksByPlatform'][p]['url'])
-        return links
-    except Exception as e:
-        print("Error get_links : " + str(e))
-        return None
+        except Exception:
+            print("Error get_links for " + p)
+            links.append("No link found")
+    return links
 
 
 def create_response(links_list):
-    if links_list == None:
-        response = "Error in API call, try with another link."
-        print(response)
-    else:
-        response = ""
-        for p, l in zip(PLATFORMS, links_list):
-            response += p+': '+l+"\n"
-        print(response)
+    response = "".join(p + ': ' + l + "\n" for p, l in zip(PLATFORMS, links_list))
+    print(response)
     return response
+
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 
-@bot.command(name='sl', help='Give a music link and get all other plateform link.\nType !sl <link> to get your links.')
+@bot.command(name='sl', help='Give a music link and get all other platform link.\nType !sl <link> to get your links.')
 async def sl(ctx, link):
     link_list = get_links(link)
     response = create_response(link_list)
@@ -49,4 +44,3 @@ async def sl(ctx, link):
 
 
 bot.run(TOKEN)
-        
